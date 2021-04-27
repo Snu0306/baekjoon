7f01a9575b231b83a9c0f05cf4b4bc87e959f332
snu0306 <psunu0306@gmail.com>
2021-04-27 16:54:25 +0900

    baek from office

--- Main.java
+++ Main.java
@@ -2,33 +2,57 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.Map;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String args[]) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		while(true) {
-			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-			int i = Integer.parseInt(st.nextToken());
-			int j = Integer.parseInt(st.nextToken());
+		
+		Queue<Integer> ans = new LinkedList<>();
+
+		int N = Integer.parseInt(st.nextToken());
+		
+		while(N-->0) {
+			StringBuilder sb = new StringBuilder();
+			st = new StringTokenizer(br.readLine());
+			int papers = Integer.parseInt(st.nextToken()); //문서의 개수
+			int order = Integer.parseInt(st.nextToken()); // 궁금한 문서가 몇번째 놓여있는지
+			int imp = 0; //그 문서의 중요도
 			
+			st = new StringTokenizer(br.readLine()); //문서 인덱스 별로 중요도
 			
-			if(i==0 && j==0) break;			
-			if(i%j!=0 && j%i !=0) {
-				sb.append("neither \n");
-			}else if(i%j ==0 && j%i !=0) {
-				sb.append("multiple \n");
-			}else {
-				sb.append("factor \n");
+		
+			for(int i=0; i<papers; i++) {
+				int temp = Integer.parseInt(st.nextToken());
+				ans.offer(temp);
+				if(i==order) imp =temp;
 			}
 			
 			
+			for(int i=0; i<papers; i++) {
+				int temp = ans.poll();
+				if(temp > imp) {
+					ans.add(temp);
+				}
+				for(int s : ans) {
+					sb.append(s+" ");
+					
+				}
+				sb.append("\n");
+			}
+			System.out.println(sb);
+			
+			
+			
+			
+			
+			
 		}
-		
-		System.out.println(sb);
 		
 	}
 
